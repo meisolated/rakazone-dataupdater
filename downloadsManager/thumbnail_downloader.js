1408c1c8c99c9c5146af1b969a873737e09d4534
meisolated <isolatedbot@gmail.com>
2022-09-17 05:47:33 +0530

    not many changes

--- downloadsManager/thumbnail_downloader.js
+++ downloadsManager/thumbnail_downloader.js
@@ -1,41 +1,63 @@
+import fs from "fs"
 import https from "https"
 import { Transform } from "stream"
-import fs from "fs"
 
 import { Videos } from "../models/Videos.model.js"
 
 const videosList = await Videos.findAll({ raw: true })
 
 videosList.map(async (video, index) => {
- // http://i.ytimg.com/vi/xqann191kbY/maxresdefault.jpg
- // https://img.youtube.com/vi/xqann191kbY/mqdefault.jpg
- // https://img.youtube.com/vi/xqann191kbY/sddefault.jpg
- // https://img.youtube.com/vi/xqann191kbY/hqdefault.jpg
- // https://img.youtube.com/vi/xqann191kbY/default.jpg
- // https://img.youtube.com/vi/xqann191kbY/0.jpg
- // https://img.youtube.com/vi/xqann191kbY/1.jpg
- // https://img.youtube.com/vi/xqann191kbY/2.jpg
- // https://img.youtube.com/vi/xqann191kbY/3.jpg
- // https://img.youtube.com/vi/xqann191kbY/4.jpg'
- let baseUrl = "https://img.youtube.com/vi/"
- let thumbnails = ["maxresdefault", "mqdefault", "sddefault", "hqdefault", "default", "0", "1", "2", "3", "4"]
+    // http://i.ytimg.com/vi/xqann191kbY/maxresdefault.jpg
+    // https://img.youtube.com/vi/xqann191kbY/mqdefault.jpg
+    // https://img.youtube.com/vi/xqann191kbY/sddefault.jpg
+    // https://img.youtube.com/vi/xqann191kbY/hqdefault.jpg
+    // https://img.youtube.com/vi/xqann191kbY/default.jpg
+    // https://img.youtube.com/vi/xqann191kbY/0.jpg
+    // https://img.youtube.com/vi/xqann191kbY/1.jpg
+    // https://img.youtube.com/vi/xqann191kbY/2.jpg
+    // https://img.youtube.com/vi/xqann191kbY/3.jpg
+    // https://img.youtube.com/vi/xqann191kbY/4.jpg'
 
- thumbnails.map(async (thumbnail, index) => {
-  const request = https.request(baseUrl + video.videoId + "/" + thumbnail + ".jpg", function (response) {
-   var data = new Transform()
+    let baseUrl = "https://img.youtube.com/vi/"
+    let thumbnails = ["maxresdefault", "mqdefault", "sddefault", "hqdefault", "default", "0", "1", "2", "3", "4"]
 
-   response.on("data", function (chunk) {
-    data.push(chunk)
-   })
-   response.on("end", function () {
-    fs.writeFileSync(`./src/downloads/videoThumbnails/${video.videoId + "-" + thumbnail}.png`, data.read())
-    console.log(video.videoId + " " + thumbnail + " downloaded")
-   })
-  })
+    thumbnails.map(async (thumbnail, index) => {
+        const request = https.request(baseUrl + video.videoId + "/" + thumbnail + ".jpg", function (response) {
+            var data = new Transform()
 
-  request.end()
-  request.on("error", (e) => {
-   console.log(e)
-  })
- })
+            response.on("data", function (chunk) {
+                data.push(chunk)
+            })
+            response.on("end", function () {
+                fs.writeFileSync(`./downloads/videoThumbnails/${video.videoId + "-" + thumbnail}.jpg`, data.read())
+                console.log(video.videoId + " " + thumbnail + " downloaded")
+            })
+        })
+
+        request.end()
+        request.on("error", (e) => {
+            console.log(e)
+        })
+    })
+
+
+    //  ____________________________________________________________________
+    //  https://i.ytimg.com/vi/oZJLyWLUWrc/hq720.jpg
+
+    const request = https.request(`https://i.ytimg.com/vi/${video.videoId}/hq720.jpg`, function (response) {
+        var data = new Transform()
+
+        response.on("data", function (chunk) {
+            data.push(chunk)
+        })
+        response.on("end", function () {
+            fs.writeFileSync(`./downloads/videoThumbnails/${video.videoId + "-hq720"}.jpg`, data.read())
+            console.log(video.videoId + " " + "hq720" + " downloaded")
+        })
+    })
+
+    request.end()
+    request.on("error", (e) => {
+        console.log(e)
+    })
 })
