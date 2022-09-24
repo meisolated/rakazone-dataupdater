9197ac5b312f8d5460ebca5160fbcf8cbd4fabd3
meisolated <isolatedbot@gmail.com>
2022-09-24 08:01:38 +0530

    thumbnail downloader works

--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
-import { downloadThumbnail } from './workers/localizeVideo/thumbnailDownloader'
+import { downloadThumbnail } from "./workers/localizeVideo/thumbnailDownloader"
 console.log("1")
 downloadThumbnail("RkkdYdWMfQ0", "/home/isolated/rakazone/rakazone-dataupdater/log/")
 console.log("2")
--- src/workers/localizeVideo/thumbnailDownloader.ts
+++ src/workers/localizeVideo/thumbnailDownloader.ts
@@ -1,54 +1,27 @@
 import fs from "fs"
-import https from "https"
-import Transform from "stream"
+import request from "request"
 import logger from "../../util/logger"
 
 const baseUrl1 = (videoId: string) => `https://i.ytimg.com/vi/${videoId}/hq720.jpg`
 const baseUrl2 = "https://img.youtube.com/vi/"
 const baseUrl2ThumbnailTypes = ["maxresdefault", "mqdefault", "sddefault", "hqdefault", "default", "0", "1", "2", "3", "4"]
 
+var download = (uri: string, outDir: string, callback: any) => {
+    request.head(uri, function (err: any, res: any, body: any) {
+        if (res.statusCode == 404) return console.log(false, uri)
+        request(uri).pipe(fs.createWriteStream(outDir)).on("close", callback)
+    })
+}
+
 export const downloadThumbnail = (videoId: string, outDir: string) =>
     new Promise(async (resolve, reject) => {
         const availableThumbs: Array<string> = []
-        const request1 = https.request(baseUrl1(videoId), (response) => {
-            if (response.statusCode == 404) return
-            availableThumbs.push("hq720")
-            var data = new Transform()
-            response.on("data", (chunk) => {
-                data.push(chunk)
-            })
-            response.on("end", () => {
-                fs.writeFileSync(`${outDir}${videoId}-hq720.jpg`, data.read())
-            })
-        })
-        request1.end()
-        request1.on("error", (e) => {
-            logger.info(`Error while trying to download ${videoId} thumbnail | hq720.jpg`)
-        })
+        if (!fs.existsSync(`${outDir}${videoId}/`)) fs.mkdirSync(`${outDir}${videoId}/`)
+        download(baseUrl1(videoId), `${outDir}${videoId}/hq720.jpg`, () => {})
         Promise.all(
             baseUrl2ThumbnailTypes.map((type) => {
-                // -----------------------------------------.....................------------------------------//
-                const request = https.request(`${baseUrl2}${videoId}/${type}.jpg`, (response) => {
-                    if (response.statusCode == 404) return
-                    availableThumbs.push(type)
-                    let data = new Transform()
-                    response.on("data", (chunk) => {
-                        // @ts-ignore
-                        data.push(chunk)
-                    })
-                    response.on("end", () => {
-                        // @ts-ignore
-                        fs.writeFileSync(`${outDir}${videoId}-${type}.jpg`, data.read())
-                    })
-                })
-                request.end()
-                request.on("error", (e) => {
-                    logger.info(`Error while trying to download ${videoId} thumbnail | ${type}.jpg `)
-                })
+                download(`${baseUrl2}${videoId}/${type}.jpg`, `${outDir}${videoId}/${type}.jpg`, () => {})
             })
         )
-
         resolve({ status: "downloaded", availableThumbs })
     })
-
-
